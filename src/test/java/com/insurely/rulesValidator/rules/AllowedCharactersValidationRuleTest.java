617b86d8cc7ee08ab49d8c10da53f25cc77dc02b
shivamhelicon <shivam.oberoi@helicon.ai>
2024-03-19 07:51:49 +0100

    adding correct structure for test

---
+++ src/test/java/com/insurely/rulesValidator/rules/AllowedCharactersValidationRuleTest.java
@@ -0,0 +1,22 @@
+package com.insurely.rulesValidator.rules;
+
+import com.insurely.rulesValidator.rules.AllowedCharactersValidationRule;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AllowedCharactersValidationRuleTest {
+
+    @Test
+    public void testValidSpecialChar() {
+        var rule = new AllowedCharactersValidationRule<>("[a-zA-ZåäöÅÄÖ ]+");
+        assertTrue(rule.apply("Jönköping Ångström"));
+    }
+
+    @Test
+    public void testInvalidSpecialChar() {
+        var rule = new AllowedCharactersValidationRule<>("åäöÅÄÖ");
+        assertFalse(rule.apply("John!Doe"));
+    }
+}
---
+++ src/test/java/com/insurely/rulesValidator/rules/LuhnValidationValidationRuleTest.java
@@ -0,0 +1,22 @@
+package com.insurely.rulesValidator.rules;
+
+import com.insurely.rulesValidator.rules.LuhnValidationValidationRule;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class LuhnValidationValidationRuleTest {
+
+    @Test
+    public void testValidLuhn() {
+        var rule = new LuhnValidationValidationRule<>(10);
+        assertTrue(rule.apply("199001255570"));
+    }
+
+    @Test
+    public void testInvalidLuhn() {
+        var rule = new LuhnValidationValidationRule<>(10);
+        assertFalse(rule.apply("199001255571"));
+    }
+}
